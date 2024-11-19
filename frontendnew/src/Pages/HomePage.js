b506212d27f786750284c45f07492a1f66a658f2
Pratik Jindal <117007380+jindalpratik@users.noreply.github.com>
2024-11-19 17:06:22 +0530

    init

---
+++ frontendnew/src/Pages/HomePage.js
@@ -0,0 +1,88 @@
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
+
+const HomePage = () => {
+
+
+
+  // const navigate = useNavigate();
+
+
+  
+
+
+//   useEffect(()=>{
+//     const user =JSON.parse(localStorage.getItem("userInfo"));
+
+
+//     if(!user){
+
+//         navigate("/");
+
+
+//     }
+// },[navigate]);
+
+
+  return (
+    // <>
+    // <div className="main-login">
+
+    //     <div className="logo-container">
+    //     {/* <img className='logo-login' src={logo}  alt="" /> */}
+
+    //     </div>
+
+    
+    //     <div className="login-container">
+
+    //     <h2>Signup To join</h2>
+
+
+
+       
+
+    //   <input className='username-search'
+    //     onChange={(e)=>setEmail(e.target.value)}
+    //     type="text" 
+    //     placeholder='email' />
+
+
+    //   <div className="password-div">
+    //   <input className='username-searchpass' type={show?("text"):("password")} 
+    //            placeholder='Password'
+    //            onChange={(e)=>setPassword(e.target.value)}  />
+    //            <button className='show' onClick={()=> setShow(!show)}>
+    //            {show?( <VisibilityIcon className='eye' />):(<VisibilityOffIcon className='eye'/>)}
+    //            </button>
+      
+    //   </div>
+       
+
+      
+
+
+
+
+               
+
+
+
+    //     <Button className='login-button' variant="contained" onClick={submitHandler} >Login</Button>
+
+    //     </div>
+
+
+    // </div>
+    
+
+
+    
+    
+    
+    // </>
+    <></>
+  )
+}
+
+export default HomePage
