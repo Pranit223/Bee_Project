ef8efe55c82da67e1c63ef0511067f5662021007
Pratik Jindal <117007380+jindalpratik@users.noreply.github.com>
2024-11-27 17:29:27 +0530

    Fix cors

--- Backend/Server.js
+++ Backend/Server.js
@@ -34,7 +34,7 @@ const server = app.listen(PORT, console.log(`Server started at port ${PORT}`));
 const io = require("socket.io")(server, {
     pingTimeout: 60000,
     cors: {
-        origin: "https://bee-project-blond.vercel.app/",
+        origin: "https://bee-project-1.onrender.com/",
     },
 });
 
